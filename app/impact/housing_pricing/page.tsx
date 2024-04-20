11401bb17bb8c276f5ba818c80e6a1d76626f3ea
Robert Barkalov <robertbarkalov@Roberts-MacBook-Air.local>
2024-04-20 12:51:01 -0400

    added graph for impact of index nsa to page, need to add text summary on both

--- app/impact/housing_pricing/page.tsx
+++ app/impact/housing_pricing/page.tsx
@@ -1,13 +1,48 @@
 'use client';
 import React, { useRef, useEffect } from 'react';
 import Chart from 'chart.js/auto';
+import { labelsArrayHousingPrices } from './labelsArrayHousingPricing';
+import { dataHousingPrices } from './dataHousingPricing';
+
+let chartInstance = null;
 
 const Housing_Pricing: React.FC = () => {
     const canvasRef = useRef(null);
 
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (canvas && !chartInstance) {
+            // Create a new Chart instance only if it doesn't exist
+            chartInstance = new Chart(canvas, {
+                type: 'scatter',
+                data: {
+                    labels: labelsArrayHousingPrices,
+                    datasets: [
+                        {
+                            label: 'index nsa',
+                            data: dataHousingPrices,
+                            borderWidth: 1,
+                        },
+                    ],
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                        },
+                    },
+                    responsive: false,
+                    maintainAspectRatio: false,
+                },
+            });
+        }
+    }, []);
+
+
     return (
         <div>
-            <h1>Impact on Interest Rates from Natural Disasters</h1>
+            <h1>Impact on index NSA from Natural Disasters</h1>
+            <canvas ref={canvasRef} id="myChart" width="600" ></canvas>
         </div>
     );
 };
